092c9c08d65aea0a6bc8513735094eb9d3c1d5de
reciokaue <kaue.recio2@gmail.com>
2024-03-13 07:24:29 -0300

    delete topics route

--- app/api/topic/route.ts
+++ app/api/topic/route.ts
@@ -43,3 +43,18 @@ export async function POST(req: NextRequest) {
 
   return Response.json({ status: 201 })
 }
+
+export async function DELETE(req: NextRequest) {
+  const { topics } = await req.json()
+
+  if (topics.length === 0)
+    return Response.json({ message: 'missing data' }, { status: 400 })
+
+  await prisma.topic.deleteMany({
+    where: {
+      name: { in: topics },
+    },
+  })
+
+  return Response.json({ status: 200 })
+}
