3fc69b8db21e8279346ca62b9fd43bc98f7bbb61
reciokaue <kaue.recio2@gmail.com>
2024-03-18 20:23:21 -0300

    card word break fix

--- components/question-card/types/cards.tsx
+++ components/question-card/types/cards.tsx
@@ -14,12 +14,12 @@ export function Cards({ options }: CardsProps) {
     <div className="grid grid-cols-2 content-center gap-3">
       {options.map((option) => (
         <button
-          className="flex h-48 w-36 flex-col items-center justify-center gap-2 whitespace-nowrap text-wrap rounded-2xl bg-background  p-4 font-semibold text-secondary-foreground shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
+          className="flex h-48 w-36 flex-col  items-center justify-center gap-2 rounded-2xl bg-background  p-4 font-semibold text-secondary-foreground shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring"
           onClick={(e) => e.preventDefault()}
           key={`${option.id}-card`}
         >
           <span className="h-12 text-5xl">{option.emoji || ''}</span>
-          <p className="h-12">{option.text}</p>
+          <p className="min-h-12 break-all">{option.text}</p>
         </button>
       ))}
     </div>
