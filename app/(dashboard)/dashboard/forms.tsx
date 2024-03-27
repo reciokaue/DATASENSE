289e95e18173273c09c0ebccac743ff76302c92b
reciokaue <kaue.recio2@gmail.com>
2024-03-27 12:16:57 -0300

    dashboard forms useQuery

---
+++ app/(dashboard)/dashboard/forms.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+
+import { FormCard } from '@/components/form-card'
+import { api } from '@/lib/api'
+
+export function Forms() {
+  const { data: forms } = useQuery({
+    queryKey: ['user-forms'],
+    queryFn: async () => {
+      const response = await api.get(`/forms`, {
+        params: { pageSize: 6 },
+      })
+      return response.data
+    },
+  })
+  return (
+    <div className="mt-10 grid grid-cols-1 gap-3 md:grid-cols-3">
+      {forms &&
+        forms.map((form: any) => <FormCard data={form} key={form.id} />)}
+    </div>
+  )
+}
--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,18 +1,12 @@
-import { FormCard } from '@/components/form-card'
 import { SearchBar } from '@/components/search-bar'
-import { api } from '@/lib/api'
+
+import { Forms } from './forms'
 
 export default async function Dashboard() {
-  const forms = await api.get('/form')
-
   return (
     <>
       <SearchBar />
-      <div className="mt-10 grid grid-cols-1 gap-3 md:grid-cols-3">
-        {forms.data.map((form: any) => (
-          <FormCard data={form} key={form.id} />
-        ))}
-      </div>
+      <Forms />
     </>
   )
 }
