9036937b71c5890f359222bb45d5d74bacf16e57
reciokaue <kaue.recio2@gmail.com>
2024-08-22 10:22:23 -0300

    sidebar

--- src/app/(logged)/layout.tsx
+++ src/app/(logged)/layout.tsx
@@ -5,10 +5,10 @@ import { Sidebar } from '@/src/components/sidebar'
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <main className="mx-auto flex h-screen w-full flex-col ">
-      <header className="flex h-20 w-full items-center px-6 ">
+      <header className="flex h-24 w-full items-center px-6 ">
         <h1 className=" text-2xl font-bold">DATASENSE</h1>
       </header>
-      <div className="flex h-full w-full bg-primary/5">
+      <div className="flex h-full w-full bg-primary-foreground">
         <Sidebar />
         <div className="mx-auto  flex h-full w-full max-w-screen-xl flex-col rounded-tl-md  p-6 ">
           {children}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,28 +1,41 @@
+'use client'
+/* eslint-disable jsx-a11y/role-supports-aria-props */
 // interface SidebarProps {}
 
 import { Book, LogOut, PieChart, Settings } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const links = [
+  { title: 'Dashboard', path: '/dashboard', icon: Book },
+  { title: 'Analytics', path: '/analytics', icon: PieChart },
+  { title: 'Configurações', path: '/settings', icon: Settings },
+  { title: 'Forms', path: '/forms', icon: Book },
+  { title: 'Logout', path: '/logout', icon: LogOut },
+]
 
 export function Sidebar() {
+  const pathname = usePathname()
+
   return (
-    <aside className="flex w-80 flex-col space-y-5 bg-white px-3">
-      <section>
-        <ul className="list-none space-y-3">
-          <li className="flex w-full cursor-pointer items-center gap-2 rounded-md p-3 font-medium text-primary/70 transition-colors hover:bg-primary/10 aria-selected:bg-primary aria-selected:text-white">
-            <Book className="size-4" /> Dashboard
-          </li>
-          <li className="flex w-full cursor-pointer items-center gap-2 rounded-md p-3 font-medium text-primary/70 transition-colors hover:bg-primary/10 aria-selected:bg-primary aria-selected:text-white">
-            <PieChart className="size-4" /> Analytics
-          </li>
-          <li className="flex w-full cursor-pointer items-center gap-2 rounded-md p-3 font-medium text-primary/70 transition-colors hover:bg-primary/10 aria-selected:bg-primary aria-selected:text-white">
-            <Settings className="size-4" /> Configurações
-          </li>
-          <li className="flex w-full cursor-pointer items-center gap-2 rounded-md p-3 font-medium text-primary/70 transition-colors hover:bg-primary/10 aria-selected:bg-primary aria-selected:text-white">
-            <Book className="size-4" /> Forms
-          </li>
-          <li className="flex w-full cursor-pointer items-center gap-2 rounded-md p-3 font-medium text-primary/70 transition-colors hover:bg-primary/10 aria-selected:bg-primary aria-selected:text-white">
-            <LogOut className="size-4" /> Logout
-          </li>
-        </ul>
+    <aside className="flex w-80 flex-col  bg-white pl-3 pr-6">
+      <section className="flex flex-col space-y-2">
+        {links.map((link) => {
+          const Icon = link.icon
+          const isActive = pathname === link.path
+
+          return (
+            <Link href={link.path} key={link.path}>
+              <li
+                className="flex w-full cursor-pointer items-center gap-2 rounded-md p-3 font-medium text-primary/70 transition-colors hover:bg-primary/10 aria-selected:bg-primary aria-selected:text-white"
+                aria-selected={isActive}
+              >
+                <Icon className="size-4" />
+                {link.title}
+              </li>
+            </Link>
+          )
+        })}
       </section>
     </aside>
   )
