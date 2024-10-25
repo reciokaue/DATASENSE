4f65b4ed17656bdea0fb90dafcf4bd0f3d0df4df
reciokaue <kaue.recio2@gmail.com>
2024-10-25 16:40:48 -0300

    settings screen, defaults toggle

--- src/app/(logged)/forms/[id]/settings/page.tsx
+++ src/app/(logged)/forms/[id]/settings/page.tsx
@@ -6,8 +6,8 @@ import { Eye } from 'lucide-react'
 import { getForm } from '@/src/api/get-form'
 import { Button } from '@/src/components/ui/button'
 import { Separator } from '@/src/components/ui/separator'
-import { Switch } from '@/src/components/ui/switch'
 
+import { Defaults } from './defaults'
 import { Description } from './description'
 import ImagePicker from './image-picker'
 import { Topics } from './topics'
@@ -42,24 +42,7 @@ export default function SettingsPage({ params }: { params: { id: string } }) {
       <Separator />
       <section className="flex flex-col space-y-4 p-3">
         <h2 className="text-xl font-normal">Padrões</h2>
-        <div className="relative">
-          <label className="text-lg font-normal">Ativo</label>
-          <p className="w-4/5 text-sm font-light leading-relaxed">
-            Define se o formulário esta ativo e pode receber novas respostas,
-            formulários inativos não podem receber novas respostas.
-          </p>
-          <Switch className="absolute right-5 top-1/2" />
-        </div>
-        <div className="relative ">
-          <label className="text-lg font-normal">Publico</label>
-          <p className="w-4/5 text-sm font-light leading-relaxed">
-            Define se o formulário pode ser acessado na aba de comunidade por
-            outros usuários, essa ação ajuda outros usuários com a criação de
-            seus formulários e não compartilha nenhum dado sensível ou respostas
-            de seu formulário.
-          </p>
-          <Switch className="absolute right-5 top-1/2" />
-        </div>
+        <Defaults form={form} />
       </section>
 
       {/* <section className="flex flex-col space-y-4 p-3">
