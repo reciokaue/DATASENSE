35d9fe804deae81b92653f7b19d92d037df69b1c
reciokaue <kaue.recio2@gmail.com>
2024-10-30 15:56:06 -0300

    switch receiving state

--- src/components/ui/switch.tsx
+++ src/components/ui/switch.tsx
@@ -17,11 +17,13 @@ const Switch = React.forwardRef<
   <Controller
     control={control}
     name={name}
-    render={(field: any) => {
-      if (field === undefined) field?.field?.onChange(false)
+    render={(required) => {
+      // if (field === undefined) field?.field?.onChange(false)
 
       return (
         <SwitchPrimitives.Root
+          checked={required.field.value || false}
+          onCheckedChange={required.field.onChange}
           className={cn(
             'peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
             className,
