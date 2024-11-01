6f11844b31f08e4318ca2f0f364b1bfc2f020b4a
reciokaue <kaue.recio2@gmail.com>
2024-11-01 19:56:55 -0300

    zod errors

--- src/models/Form.ts
+++ src/models/Form.ts
@@ -10,7 +10,7 @@ import { QuestionSchema, questionSelect } from './Question'
 export const FormSchema = z.object({
   id: z.number().int(),
   name: z.string().min(5, { message: 'No mínimo 5 caracteres' }),
-  description: z.string({ message: 'Campo obrigatório' }),
+  description: z.string({ required_error: 'Campo obrigatório' }),
   active: z.boolean().default(true),
   logoUrl: z.string().nullable(),
   isPublic: z.boolean().nullable().default(true),
