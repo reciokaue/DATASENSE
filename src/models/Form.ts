29922b4c10716263c8d501737e9754446e79da73
reciokaue <kaue.recio2@gmail.com>
2024-11-02 16:54:17 -0300

    zod

--- src/models/Form.ts
+++ src/models/Form.ts
@@ -11,7 +11,6 @@ export const FormSchema = z.object({
   id: z.number().int(),
   name: z.string().min(5, { message: 'No mínimo 5 caracteres' }),
   description: z.string({ required_error: 'Campo obrigatório' }),
-
   active: z.boolean().default(true),
   logoUrl: z.string().nullable(),
   isPublic: z.boolean().nullable().default(true),
