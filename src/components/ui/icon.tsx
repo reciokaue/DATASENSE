a04da826a1351727baa4d32d986e609140a1fde4
reciokaue <kaue.recio2@gmail.com>
2024-11-03 20:00:59 -0300

    generic icon

--- src/components/ui/icon.tsx
+++ src/components/ui/icon.tsx
@@ -2,17 +2,19 @@
 // @ts-nocheck
 
 import { Circle, LucideProps } from 'lucide-react'
-// import dynamicIconImports from 'lucide-react/dynamicIconImports'
-// import dynamic from 'next/dynamic'
+import dynamicIconImports from 'lucide-react/dynamicIconImports'
+import dynamic from 'next/dynamic'
 
 interface IconProps extends LucideProps {
   name?: string
 }
 
 export const Icon = ({ name, ...props }: IconProps) => {
-  // const LucideIcon = dynamic(dynamicIconImports[name])
-
-  // return <LucideIcon {...props} />
-  return <Circle {...props} />
+  if (process.env.NEXT_PUBLIC_ENVIRONMENT !== 'DEV') {
+    const LucideIcon = dynamic(dynamicIconImports[name])
+    return <LucideIcon {...props} />
+  } else {
+    return <Circle {...props} />
+  }
 }
 Icon.displayName = 'Icon'
