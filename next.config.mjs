bf173ada31861969e13aadaaf350566b85413833
reciokaue <kaue.recio2@gmail.com>
2024-11-12 17:41:09 -0300

    content layer install and config

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   pageExtensions: ['ts', 'tsx'],
   transpilePackages: ['lucide-react'],
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['encrypted-tbn0.gstatic.com'],
   },
