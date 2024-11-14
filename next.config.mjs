494116213a306403b092616e3ab5907d6abea5fb
reciokaue <kaue.recio2@gmail.com>
2024-11-14 11:38:05 -0300

    content layer config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  pageExtensions: ['ts', 'tsx'],
-  transpilePackages: ['lucide-react'],
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: ['encrypted-tbn0.gstatic.com'],
-  },
-}
+import { withContentlayer } from 'next-contentlayer'
 
-export default nextConfig
+/** @type {import('next').NextConfig} */
+export default withContentlayer({
+  reactStrictMode: true,
+})
