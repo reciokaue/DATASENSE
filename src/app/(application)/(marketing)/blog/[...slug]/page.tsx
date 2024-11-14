c6729ea3c7b3f53b946fa5b830c18507822b1c8e
reciokaue <kaue.recio2@gmail.com>
2024-11-14 11:38:24 -0300

    blog/landing pages

---
+++ src/app/(application)/(marketing)/blog/[...slug]/page.tsx
@@ -0,0 +1,164 @@
+import '@/styles/mdx.css'
+
+import { allAuthors, allPosts } from 'contentlayer/generated'
+import { ChevronLeft } from 'lucide-react'
+import { Metadata } from 'next'
+import Image from 'next/image'
+import Link from 'next/link'
+import { notFound } from 'next/navigation'
+
+import { Mdx } from '@/components/md/mdx-components'
+import { Button, buttonVariants } from '@/components/ui/button'
+import { absoluteUrl, cn, formatDate } from '@/lib/utils'
+
+interface PostPageProps {
+  params: {
+    slug: string[]
+  }
+}
+
+async function getPostFromParams(params) {
+  const slug = params?.slug?.join('/')
+  const post = allPosts.find((post) => post.slugAsParams === slug)
+
+  if (!post) {
+    return null
+  }
+
+  return post
+}
+
+export async function generateMetadata({
+  params,
+}: PostPageProps): Promise<Metadata> {
+  const post = await getPostFromParams(params)
+
+  if (!post) {
+    return {}
+  }
+
+  const url = process.env.NEXT_PUBLIC_APP_URL
+
+  const ogUrl = new URL(`${url}/api/og`)
+  ogUrl.searchParams.set('heading', post.title)
+  ogUrl.searchParams.set('type', 'Blog Post')
+  ogUrl.searchParams.set('mode', 'dark')
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: post.authors.map((author) => ({
+      name: author,
+    })),
+    openGraph: {
+      title: post.title,
+      description: post.description,
+      type: 'article',
+      url: absoluteUrl(post.slug),
+      images: [
+        {
+          url: ogUrl.toString(),
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: post.title,
+      description: post.description,
+      images: [ogUrl.toString()],
+    },
+  }
+}
+
+export async function generateStaticParams(): Promise<
+  PostPageProps['params'][]
+> {
+  return allPosts.map((post) => ({
+    slug: post.slugAsParams.split('/'),
+  }))
+}
+
+export default async function PostPage({ params }: PostPageProps) {
+  const post = await getPostFromParams(params)
+
+  if (!post) {
+    notFound()
+  }
+
+  const authors = post.authors.map((author) =>
+    allAuthors.find(({ title }) => {
+      console.log(title, author)
+      return title === author.slice(0, -1)
+    }),
+  )
+
+  return (
+    <article className="container relative flex max-w-3xl flex-col py-6 lg:py-10">
+      <Button link="/blog" variant="ghost" className=" mb-6">
+        <ChevronLeft className="mr-2 h-4 w-4" />
+        Ver todas as publicações
+      </Button>
+      <div>
+        {post.date && (
+          <time
+            dateTime={post.date}
+            className="block text-sm text-muted-foreground"
+          >
+            Publicado em {formatDate(post.date)}
+          </time>
+        )}
+        <h1 className="font-heading mt-2 inline-block text-4xl leading-tight lg:text-5xl">
+          {post.title}
+        </h1>
+        {authors?.length ? (
+          <div className="mt-4 flex space-x-4">
+            {authors.map((author) =>
+              author ? (
+                <Link
+                  key={author._id}
+                  href={`https://twitter.com/${author.twitter}`}
+                  className="flex items-center space-x-2 text-sm"
+                >
+                  <Image
+                    src={author.avatar}
+                    alt={author.title}
+                    width={42}
+                    height={42}
+                    className="rounded-full bg-white"
+                  />
+                  <div className="flex-1 text-left leading-tight">
+                    <p className="font-medium">{author.title}</p>
+                    <p className="text-[12px] text-muted-foreground">
+                      @{author.twitter}
+                    </p>
+                  </div>
+                </Link>
+              ) : null,
+            )}
+          </div>
+        ) : null}
+      </div>
+      {post.image && (
+        <Image
+          src={post.image}
+          alt={post.title}
+          width={720}
+          height={405}
+          className="my-8 rounded-md border bg-muted transition-colors"
+          priority
+        />
+      )}
+      <Mdx code={post.body.code} />
+      <hr className="mt-12" />
+      <div className="flex justify-center py-6 lg:py-10">
+        <Link href="/blog" className={cn(buttonVariants({ variant: 'ghost' }))}>
+          <ChevronLeft className="mr-2 h-4 w-4" />
+          Ver todas as publicações
+        </Link>
+      </div>
+    </article>
+  )
+}
---
+++ src/app/(application)/(marketing)/page.tsx
@@ -0,0 +1,94 @@
+import { ClipboardPlus } from 'lucide-react'
+import Link from 'next/link'
+
+import { buttonVariants } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
+
+export default function IndexPage() {
+  return (
+    <>
+      <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
+        <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
+          <Link
+            href={'#'}
+            className="rounded-2xl bg-muted px-4 py-1.5 text-sm font-medium"
+            target="_blank"
+          >
+            Acompanhe no GitHub
+          </Link>
+          <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+            Datasense: Sistema de Coleta e Análise de Dados por Formulários
+            Online
+          </h1>
+          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            Um projeto para simplificar a coleta de dados e oferecer insights
+            precisos, mesmo sem experiência prévia, a um custo acessível.
+          </p>
+          <div className="space-x-4">
+            <Link href="/login" className={cn(buttonVariants({ size: 'lg' }))}>
+              Comece Agora
+            </Link>
+            <Link
+              href={'#'}
+              target="_blank"
+              rel="noreferrer"
+              className={cn(buttonVariants({ variant: 'outline', size: 'lg' }))}
+            >
+              GitHub
+            </Link>
+          </div>
+        </div>
+      </section>
+
+      <section
+        id="features"
+        className="container space-y-6 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
+      >
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+            Funcionalidades
+          </h2>
+          <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+            Um sistema completo para criar formulários, captar respostas e gerar
+            análises de forma prática e acessível.
+          </p>
+        </div>
+        <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
+          <FeatureCard
+            title="Criação de Formulários"
+            description="Escolha modelos prontos, edite perguntas e personalize seus formulários para coleta de dados."
+          />
+          <FeatureCard
+            title="Análises Detalhadas"
+            description="Acompanhe os resultados em tempo real e obtenha insights sobre clientes e colaboradores."
+          />
+          <FeatureCard
+            title="QR Code e Link de Acesso"
+            description="Distribua facilmente seus formulários por meio de QR codes e links diretos."
+          />
+          <FeatureCard
+            title="Gerenciamento de Usuários"
+            description="Controle total sobre os gerentes e formulários através de um painel administrativo."
+          />
+          <FeatureCard
+            title="Resultados Visuais"
+            description="Visualize gráficos e tabelas dos resultados coletados."
+          />
+          <FeatureCard
+            title="Planos Flexíveis"
+            description="Escolha o plano mais adequado para suas necessidades de coleta de dados."
+          />
+        </div>
+      </section>
+    </>
+  )
+}
+
+function FeatureCard({ title, description }) {
+  return (
+    <div className="flex h-[150px] flex-col space-y-2 rounded-lg bg-background p-6 ">
+      <h3 className="text-xl font-bold">{title}</h3>
+      <p className="text-sm text-muted-foreground">{description}</p>
+    </div>
+  )
+}
